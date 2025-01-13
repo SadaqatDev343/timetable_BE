ae1529cfbfcc02fafe453cacee83dd85f2e3e72a
sadaqat <msadaqatdev@gmail.com>
2025-01-13 22:06:02 +0500

    fixes

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,12 @@
 import { User } from './user/users.modal';
 
 export enum UserRole {
-    Admin = 'admin',
-    Student = 'student',
-    Teacher = 'teacher',
-    Staff = 'staff',
-  }
-  export interface IAuthResponse {
-    user: User;
-    access_Token: string;
-  }
-  
+  Admin = 'admin', //types
+  Student = 'student',
+  Teacher = 'teacher',
+  Staff = 'staff',
+}
+export interface IAuthResponse {
+  user: User;
+  access_Token: string;
+}
